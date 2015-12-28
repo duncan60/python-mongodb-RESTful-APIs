fcc0590f04753416774e13cdbc1295af20461a40
duncan60 <tmduncan60@gmail.com>
2015-12-28 22:43:37 +0800

    update books post , but not perfect

--- app/books.py
+++ app/books.py
@@ -1,22 +1,20 @@
+import json
 from flask import Flask, abort
 from flask.ext import restful
 from flask.ext.restful import reqparse
 from app import app, api, mongo
 
 class BookList(restful.Resource):
-	def __init__(self, *args, **kwargs):
- 		self.reqparse = reqparse.RequestParser()
-        #self.reqparse.add_argument('title', type = str, required = True, help = 'No task title provided', location = 'json')
-        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
-        super(BookList, self).__init__()
+    def get(self):
+        return [x for x in mongo.db.bookList.find({}, {'_id': 0})]
 
-	def get(self):
-		return [x for x in mongo.db.bookList.find({}, {'_id': 0})]
-
-	def post(self):
-		args = self.parser.parse_args()
-		jo = json.loads(args['title'], args['price'])
-		print jo
-		return {'msg': 'ok'}
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type = str)
+        parser.add_argument('price', type = str)
+        args = parser.parse_args()
+        mongo.db.bookList.insert(args)
+        #jo = json.loads(args['title'], args['price'])
+        return {'msg': 'ok'}
 
 api.add_resource(BookList, '/books')
