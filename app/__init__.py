27b5478f20d40a4865f950a779036fde11e96f40
DuDuncan <tmduncan60@gmail.com>
2015-12-30 17:05:19 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,26 @@ from flask import Flask
 from flask.ext import restful
 from flask.ext.restful import fields
 from flask.ext.pymongo import PyMongo
+from flask import make_response
+from bson.json_util import dumps
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://127.0.0.1:12345/bookList'
+MONGO_URL = 'mongodb://127.0.0.1:12345/bookList'
+
+
+app.config['MONGO_URI'] = MONGO_URL
+mongo = PyMongo(app)
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
 
 api = restful.Api(app)
-mongo = PyMongo(app)
+api.representations = DEFAULT_REPRESENTATIONS
 
 resource_fields = {
     '_id'  : fields.String,
@@ -21,4 +35,4 @@ class Index(restful.Resource):
 
 api.add_resource(Index, '/')
 
-from app import bookList, book
+from app import books, book
--- app/books.py
+++ app/books.py
@@ -2,7 +2,7 @@ from flask import abort
 from flask.ext.restful import Resource, reqparse, marshal_with
 from app import app, api, mongo, resource_fields
 
-class BookList(Resource):
+class Books(Resource):
     @marshal_with(resource_fields, envelope='bool_list')
     def get(self):
         return [x for x in mongo.db.bookList.find()]
@@ -12,7 +12,14 @@ class BookList(Resource):
         parser.add_argument('title', type = str, required = True, help = 'No task title provided')
         parser.add_argument('price', type = str, required = True, help = 'No task price provided')
         args = parser.parse_args()
+        if not args['title'] and not args['price']:
+            abort(400)
+        args = {}
+        for k, v in parser.parse_args().iteritems():
+            if k == 'price':
+                    v = float(v)
+            args[k] = v
         mongo.db.bookList.insert(args)
         return {'msg': 'create book ok'}, 200
 
-api.add_resource(BookList, '/books', endpoint = 'books')
+api.add_resource(Books, '/books', endpoint = 'books')
