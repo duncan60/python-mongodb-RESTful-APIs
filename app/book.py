fdb59f2ae4e53598bdd6562fa73927d4c5611de3
DuDuncan <tmduncan60@gmail.com>
2016-01-05 11:28:40 +0800

    modified post success staus

--- app/book.py
+++ app/book.py
@@ -27,6 +27,6 @@ class Book(Resource):
 
         args['date_updated'] = datetime.utcnow()
         mongo.db.bookList.find_one_and_update({'_id': book_id}, {'$set': args})
-        return {'msg': 'update success'}, 200
+        return {'msg': 'update success'}, 201
 
 api.add_resource(Book, '/book/<ObjectId:book_id>', endpoint = 'book')
